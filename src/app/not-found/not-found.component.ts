d4bba6ffa035e0054f2c271afb0b49ac6f7e83b9
Tejii13 <telmosousapro@proton.me>
2023-09-29 08:57:36 +0200

    Autocomplete bug minimal reproduction

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent {
-
+  public fruits: string[] = [
+    'Pineapple',
+    'Grape',
+    'Cranberry',
+    'Cherry',
+    'Orange',
+    'Watermelon',
+    'Banana',
+    'Apple',
+    'Coconut',
+  ];
 }
