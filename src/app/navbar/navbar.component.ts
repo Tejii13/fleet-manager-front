801fbcfa740488ced884971666ad73a39dc792f0
Tejii13 <telmosousapro@proton.me>
2023-10-17 09:26:50 +0200

    bug correction

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  constructor(private router: Router, private cookieService: CookieService) {}
+
+  private username!: string;
+  public isConnected: boolean = false;
+
+  ngOnInit(): void {
+    this.username = this.cookieService.get('username');
+    const authCookie = this.cookieService.get('auth');
+
+    if (
+      this.username &&
+      authCookie &&
+      this.username !== '' &&
+      authCookie !== ''
+    ) {
+      // Code here
+      console.log('Ok');
+      this.isConnected = true;
+    } else {
+      console.log('Not Ok');
+      this.disconnect();
+    }
+  }
+
+  disconnect() {
+    this.cookieService.delete('auth', '/');
+    this.cookieService.delete('username', '/');
+    this.isConnected = false;
+    this.router.navigate(['/']);
+  }
+}
