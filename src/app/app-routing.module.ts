1299778c441d0f65f65c98e2b458ec552b9169ec
Tejii13 <telmosousapro@proton.me>
2023-11-02 09:07:41 +0100

    added legal page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
-import { MonEspaceComponent } from './User-Interface/Member-Interface/mon-espace/mon-espace.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MonEspaceComponent } from './User-Interface/Member-Interface/mon-espace/mon-espace.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LegalComponent } from './legal/legal.component';
 
 const routes: Routes = [
   { path: 'accueil', component: LandingPageComponent },
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: 'mon-espace', redirectTo: '/accueil', pathMatch: 'full' },
   { path: 'mon-espace/:id', component: MonEspaceComponent },
+  { path: 'legal', component: LegalComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
